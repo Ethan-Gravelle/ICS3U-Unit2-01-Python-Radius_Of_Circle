41d629e650f067607b40aeeea8a9ff4a5d14a0bc
Ubuntu <ubuntu@ip-172-31-1-217.ec2.internal>
2021-04-28 03:04:41 +0000

    added in this file

---
+++ radius_of_circle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Ethan Gravelle
+# Created on: April 2021
+# This program does the radius of a circle
+
+import math
+
+
+def main():
+    
+    print("If a circle has a radius of 15mm: ")
+    print("")
+    print("Area is {}mm².".format(math.pi * 15 ** 2))
+    print("Perimeter is {}mm.".format(2 * math.pi *15))
+    print("")
+    
+if __name__ == "__main__":
+ main()
